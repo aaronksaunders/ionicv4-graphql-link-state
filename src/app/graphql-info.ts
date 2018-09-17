888253880c5b82172be64a781e12ae184bf32bd3
aaron saunders <aaronksaunders@gmail.com>
2018-09-17 00:08:17 -0400

    added the first query

--- src/app/graphql-info.ts
+++ src/app/graphql-info.ts
@@ -2,7 +2,10 @@ import { Apollo, gql } from 'apollo-angular-boost';
 export default {
 
   defaults: {
-    users: []
+    users: [
+      { "id": "100", "name":"Aaron", "__typename" :"User" },
+      { "id": "101", "name":"Andrea", "__typename" :"User" }
+    ]
   },
   resolvers: {
     Query: {
