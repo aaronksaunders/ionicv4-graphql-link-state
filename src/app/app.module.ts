cf536cd2eb32219bacef15b4e18217393224bc7b
aaron saunders <aaronksaunders@gmail.com>
2018-09-16 20:24:56 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, RouteReuseStrategy, Routes } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+// GRAPH QL
+import { HttpClientModule } from "@angular/common/http";
+import {
+  ApolloBoostModule,
+  ApolloBoost,
+  InMemoryCache
+} from "apollo-angular-boost";
+import GRAPH_QL from "./graphql-info";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule, // provides HttpClient for HttpLink
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ApolloBoostModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +34,20 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(
+    apolloBoost: ApolloBoost
+  ) {
+    const cache = new InMemoryCache();
+    apolloBoost.create({
+
+      clientState: {
+        cache,
+        defaults: GRAPH_QL.defaults,
+        resolvers: GRAPH_QL.resolvers
+      }
+    })
+  }
+}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular-boost';
+import { Observable } from 'rxjs';
+import { map, tap, pluck } from 'rxjs/operators';
+import { GET_USERS_QUERY, GET_USER_QUERY, ADD_USER_QUERY } from '../graphql-info'
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,57 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  task = { name: "" }
+  allUsers$
 
+  constructor(private apollo: Apollo) {
+
+  }
+
+  ngOnInit() {
+    this.allUsers$ = this.getUsers()
+  }
+
+  getUserById() {
+
+    this.apollo.watchQuery<any>({
+      query: GET_USER_QUERY,
+      variables: {
+        id: "10",
+      }
+    }).valueChanges.pipe(
+      map((data) => console.log(data.data))
+      ).subscribe();
+  }
+
+  getUsers() {
+
+
+    return this.apollo.watchQuery<any>({
+      query: GET_USERS_QUERY,
+    }).valueChanges.pipe(pluck("data", "users"));
+  }
+
+
+  addUserClicked() {
+    this.addUser(new Date().getTime() + "", this.task.name)
+  }
+
+
+  addUser(id: string, name: string) {
+
+    this.apollo.mutate<any, any>({
+      mutation: ADD_USER_QUERY,
+      variables: {
+        id,
+        name
+      }
+    }).pipe(
+      tap((data) => console.log(data.data))
+      ).subscribe();
+
+  }
 }
+
+
+
